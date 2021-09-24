d55e9f8a54bb93646f2e1d53ad1877652e5040b6
PedroSebastiaan <pizarropedrov@gmail.com>
2021-09-23 21:40:02 -0300

    add: references of wine oenologists

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,4 +2,7 @@ class Wine < ApplicationRecord
     has_many :wine_strains
     has_many :strains, through: :wine_strains
     accepts_nested_attributes_for :wine_strains, reject_if: :all_blank 
+    has_many :wine_scores
+    has_many :oenologists, through: :wine_scores
+    accepts_nested_attributes_for :wine_scores, reject_if: :all_blank 
 end
