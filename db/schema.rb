a5ff6a9240f1e2c07b9383cdb143834b332a3ee5
PedroSebastiaan <pizarropedrov@gmail.com>
2021-09-25 10:27:06 -0300

    add: year to wines

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_003300) do
+ActiveRecord::Schema.define(version: 2021_09_25_132636) do
 
   create_table "magazines", force: :cascade do |t|
     t.string "name"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_003300) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "year"
   end
 
 end
