bcc4521f9a4a46ce74160b17fe55e70b25959810
PedroSebastiaan <pizarropedrov@gmail.com>
2021-09-26 18:15:50 -0300

    add: spec to strains

--- spec/models/wine_spec.rb
+++ spec/models/wine_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Wine, type: :model do
-  it 'different strain name' do
-    wine = Wine.create(name: "Test-name", year: "test-year")
-    strains = Wine.wine_strains.create()
-  end 
+  
 end
