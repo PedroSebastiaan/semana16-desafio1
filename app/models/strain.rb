7b3e3ac47d9d31dc600298fe958b9944e420e1dc
PedroSebastiaan <pizarropedrov@gmail.com>
2021-09-26 18:35:49 -0300

    add: rspec to models

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,5 +1,6 @@
 class Strain < ApplicationRecord
     has_many :wine_strains
     has_many :wines, through: :wine_strains
-    validates :name, uniqueness: :true
+    validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+    validates_uniqueness_of :name
 end
--- spec/models/strain_spec.rb
+++ spec/models/strain_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Strain, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create a strain with nil name' do
+    strain = Strain.create(name: nil)
+    expect(strain).to_not be_valid
+  end
+
+  it 'Create a strain with no name' do
+    strain = Strain.create(name: "")
+    expect(strain).to_not be_valid
+  end
+
+  it 'Create a strain with Carmenere name' do
+    strain_1 = Strain.create(name: "Carmenere")
+    strain_2 = Strain.create(name: "Carmenere")
+    expect(strain_2).to_not be_valid
+  end
 end
